e4ed0ada22e2da88140001abdfdc9804cd55f2dc
Paul Crews <ptcrews.jr@gmail.com>
2023-12-11 17:10:38 -0800

    Finished main OD proof. Still have unfinished lemmas

--- ObservationalDeterminism.v
+++ ObservationalDeterminism.v
@@ -1,5 +1,8 @@
+From Coq Require Import Lists.List. Import ListNotations.
 From Coq Require Import Sets.Ensembles.
 From Coq Require Import Lists.Streams.
+From Coq Require Import Logic.Classical_Prop.
+From Coq Require Import Logic.Classical_Pred_Type.
 From Hyperproperties Require Import Hyperproperties.
 
 Record ODState: Type := mkODState
@@ -67,11 +70,96 @@ CoInductive EqSt_pub (t t': trace) : Prop :=
   eqst_pub:
   (hd t).(public) = (hd t').(public) -> EqSt_pub (tl t) (tl t') -> EqSt_pub t t'.
 
+Lemma EqSt_pub_eq_at_all_index:
+  forall (t t': trace),
+  EqSt_pub t t' <-> forall (i: nat), (Str_nth i t).(public) = (Str_nth i t').(public).
+Proof. Admitted.
+
 Definition ObservationalDeterminism (s: system): Prop :=
   forall (t t': trace),
   In trace s t /\ In trace s t'
   /\ (hd t).(public) = (hd t').(public) -> EqSt_pub t t'.
 
+Lemma neq_Empty_set_gives_element:
+  forall {A: Type} (s: Ensemble A),
+  s <> Empty_set A -> exists a: A, In A s a.
+Proof. Admitted.
+
+Lemma not_EqSt_pub_gives_index:
+  forall (t t': trace), ~ EqSt_pub t t' -> exists (i: nat),
+  (Str_nth i t).(public) <> (Str_nth i t').(public).
+Proof. Admitted.
+
+Fixpoint build_prefix (i: nat) (t: trace): subtrace :=
+  match i with
+  | O => []
+  | S i' => (hd t) :: build_prefix i' (tl t)
+  end.
+
+Lemma build_prefix_consistent:
+  forall (i: nat) (t: trace),
+  prefix t (build_prefix i t).
+Proof. Admitted.
+
+Lemma build_prefix_length:
+  forall (i: nat) (t: trace),
+  length (build_prefix i t) = i.
+Proof. induction i. intros. simpl. reflexivity.
+intros. simpl. f_equal. apply IHi. Qed.
+
+(* TODO: I think there's an off-by-one error here. *)
+Lemma prefix_implies_equals:
+  forall (i: nat) (t t': trace) (p: subtrace),
+  prefix t p /\ prefix t' p /\ i <= length p -> Str_nth i t = Str_nth i t'.
+Proof. Admitted.
+
 Theorem ObservationalDeterminism_is_hypersafety:
   safety_hyperproperty ObservationalDeterminism.
-Proof. Admitted.
+Proof.
+  unfold safety_hyperproperty. unfold In in *.
+  unfold not in *.
+  unfold ObservationalDeterminism in *.
+  intros.
+  apply not_all_ex_not in H. destruct H as [t H].
+  apply not_all_ex_not in H. destruct H as [t' H].
+  apply imply_to_and in H. destruct H. destruct H as [H [H' H''] ].
+  apply not_EqSt_pub_gives_index in H0. destruct H0 as [i H0].
+  exists [build_prefix i t; build_prefix i t'].
+  intros. split.
+  - unfold prefix_set. intros. inversion H1.
+    + exists t. split. apply H. subst p. apply build_prefix_consistent.
+    + inversion H2.
+      ++ exists t'. split. apply H'. subst p. apply build_prefix_consistent.
+      ++ inversion H3.
+  - intros. unfold prefix_set in H1.
+    assert (Hexistst1: exists t1: trace,
+            In trace s' t1 /\ prefix t1 (build_prefix i t)).
+    { destruct (H1 (build_prefix i t)) as [t1]. simpl. left. reflexivity.
+      destruct H3. exists t1. split; assumption. }
+    assert (Hexistst1': exists t1': trace,
+            In trace s' t1' /\ prefix t1' (build_prefix i t')).
+    { destruct (H1 (build_prefix i t')) as [t1']. simpl. right. left.
+      reflexivity. destruct H3. exists t1'. split; assumption. }
+    destruct Hexistst1 as [t1 [Hexistst1 Hexistst11] ].
+    destruct Hexistst1' as [t1' [Hexistst1' Hexistst11'] ].
+    destruct (H2 t1 t1') as [H2'].
+    + repeat split.
+      ++ apply Hexistst1.
+      ++ apply Hexistst1'.
+      ++ destruct i.
+         +++ unfold not in H0. unfold Str_nth in H0. simpl in H0.
+             apply H0 in H''. contradiction.
+         +++ inversion Hexistst11. inversion Hexistst11'.
+             subst s0 t'0 s1 t'1. simpl. apply H''.
+    + apply eqst_pub in e; try apply H2'. rewrite EqSt_pub_eq_at_all_index in e.
+      specialize (e i). pose proof prefix_implies_equals.
+      assert (Hteqt1: Str_nth i t = Str_nth i t1).
+      { apply prefix_implies_equals with (p:= (build_prefix i t)).
+        repeat split. apply build_prefix_consistent. assumption.
+        rewrite build_prefix_length. apply le_n. }
+      assert (Ht'eqt1': Str_nth i t' = Str_nth i t1').
+      { apply prefix_implies_equals with (p:= (build_prefix i t')).
+        repeat split. apply build_prefix_consistent. assumption.
+        rewrite build_prefix_length. apply le_n. }
+      rewrite Hteqt1 in H0.  rewrite Ht'eqt1' in H0. contradiction.
+Qed.
